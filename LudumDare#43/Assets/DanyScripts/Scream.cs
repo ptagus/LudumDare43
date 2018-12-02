e115caf40cda8bbd4ac8b9f54338376003e8107c
FoxWindler <gzahzah01@gmail.com>
2018-12-02 16:50:34 +0600

    Done

---
+++ LudumDare#43/Assets/DanyScripts/Scream.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scream : MonoBehaviour {
+	public AudioSource sourceScream;
+	public List<AudioClip> screams;
+
+
+	// Use this for initialization
+	void Start () {
+		sourceScream = gameObject.GetComponent<AudioSource> ();
+		//scream1 = Random.Range (0, screams.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Stanok.screamCount == true) {
+			Debug.Log ("!");
+			sourceScream.clip = screams [Random.Range (0, screams.Capacity)];
+			sourceScream.PlayOneShot (sourceScream.clip);
+			Stanok.screamCount = false;
+		}
+	}
+}
--- LudumDare#43/Assets/DanyScripts/Stanok.cs
+++ LudumDare#43/Assets/DanyScripts/Stanok.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Stanok : MonoBehaviour
 {
+	public static bool screamCount = false;
     int i = 0;
     public PlayerController Player;
 
@@ -12,12 +13,14 @@ public class Stanok : MonoBehaviour
         if(other.tag == "Detail" && other.GetComponent<IsAxtiveNow>().isActive)
         {
             i++;
+
             Player.LoseFinger(i);
         }
         //Always
         //Detail done
         //May Be
         //Lose Finger
+		screamCount = true;
         Debug.Log(other.name + "DetailDone");
     }
 
