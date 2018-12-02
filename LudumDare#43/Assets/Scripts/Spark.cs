bdfb9a8152beafc8f647e54042c6a205fe96c6ed
FoxWindler <gzahzah01@gmail.com>
2018-12-02 14:14:46 +0600

    Spark

---
+++ LudumDare#43/Assets/Scripts/Spark.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spark : MonoBehaviour {
+	public GameObject spark;
+	GameObject lamp;
+	// Use this for initialization
+	void Start () {
+		 lamp = GameObject.FindGameObjectWithTag ("Lamp");
+		StartCoroutine (SparkCreation ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SparkCreation ()
+	{
+		yield return new WaitForSeconds(Random.Range(15,30));
+		Instantiate (spark, lamp.transform.position, Quaternion.identity);
+		StartCoroutine (SparkCreation ());
+	}
+}
+
