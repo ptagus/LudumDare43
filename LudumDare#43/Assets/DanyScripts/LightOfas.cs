a670e730a7a43c4fc22151243ecc8d0a363cd921
FoxWindler <gzahzah01@gmail.com>
2018-12-02 17:44:11 +0600

    MainMenu

---
+++ LudumDare#43/Assets/DanyScripts/LightOfas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOfas : MonoBehaviour {
+	public Light light;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (light.intensity !=0) {
+			light.intensity = light.intensity - 0.01f;
+		}*/
+
+		/*while (light.intensity !=1) {
+			light.intensity = light.intensity + 0.01f;
+		}*/
+
+	}
+}
