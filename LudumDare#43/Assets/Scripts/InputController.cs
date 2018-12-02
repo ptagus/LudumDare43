5bed8a9ad07e15acea2ea048fef11dbcdeffb7a1
ptagus <ptagusss@ya.ru>
2018-12-02 11:11:18 +0600

    My
    Second

---
+++ LudumDare#43/Assets/Scripts/InputController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    bool isMove;
+    public Transform cube;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {        
+        cube.Translate(Vector3.up * Input.GetAxis("Mouse Y"));
+    }
+}
