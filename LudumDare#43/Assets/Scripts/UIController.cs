1d8a05ad54dd14cee3bcd6cb6b746897de96737c
ptagus <ptagusss@ya.ru>
2018-12-02 11:45:15 +0600

    My
    Menu

---
+++ LudumDare#43/Assets/Scripts/UIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+    public GameObject About;
+    public GameObject Settings;
+
+    public void StartGame(int sceneNumber)
+    {
+        SceneManager.LoadSceneAsync(sceneNumber);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void SaveSettings()
+    {
+        //Save some settings
+        Settings.SetActive(false);
+    }
+}
